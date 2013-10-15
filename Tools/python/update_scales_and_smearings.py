00f8f20df38a99cc2521ab051e725b239f6ea584
veverka <>
2013-10-15 11:39:05 +0000

    Added formatting of scale corrections.

--- Tools/python/update_scales_and_smearings.py
+++ Tools/python/update_scales_and_smearings.py
@@ -49,11 +49,11 @@ def parse_arguments():
 def parse_and_update_corrections(cfg):
     smearing_for_category = parse_smearings(cfg)
     smearings = format_smearings(smearing_for_category, cfg)
-    print 'Updating smearings to:', smearings
+    print 'Updating smearings to:'
+    pprint.pprint(smearings)
     update_smearings(smearings, cfg)
+
     scales_for_run_range = parse_scales(cfg)
-    print '>> scales_for_run_range:'
-    pprint.pprint(scales_for_run_range)
     scales = format_scales(scales_for_run_range, cfg)
     print 'Updating scales to:'
     pprint.pprint(scales)
@@ -82,16 +82,31 @@ def parse_smearings(cfg):
 
 #_______________________________________________________________________________
 def format_smearings(smearing_for_category, cfg):
+    '''
+    Returns the arguments of PhotonPairSelector::SetMCSmearFactors as a string
+    given cat->dsigma dictionary smearing_for_category, which maps the ECALELF
+    category name (cat) and ECALELF smearing as a string in % (dsigma).
+    Example of a smearing_for_category dictionary:
+    {'EBhighEtaBad': '1.85',
+     'EBhighEtaGold': '1.01',
+     'EBlowEtaBad': '0.96',
+     'EBlowEtaGold': '0.68',
+     'EEhighEtaBad': '1.83',
+     'EEhighEtaGold': '2.01',
+     'EElowEtaBad': '1.85',
+     'EElowEtaGold': '1.58'}
+    Example of a formatted output:
+    '0.0068, 0.0068, 0.0096, 0.0101, 0.0185, 0.0158, 0.0185, 0.0201, 0.0183'
+    '''
     mit_smearings = []
-    category_map = cfg.mit_to_ecalelf_smearing_category_map
+    ecalelf_from_mit_category = cfg.ecalelf_from_mit_smearing_category
     for mit_category in cfg.mit_smearing_categories:
-        ecalelf_category = category_map[mit_category]
+        ecalelf_category = ecalelf_from_mit_category[mit_category]
         ecalelf_smearing = smearing_for_category[ecalelf_category]
-        ## Transform, for instance from values in % to actual values, i.e.
+        ## Transform the ECALELF value convention to the MIT one,
+        ## for instance from values in % to actual values, i.e.
         ## from 0.96 to 0.0096.
-        mit_smearing = cfg.ecalelf_to_mit_smearing_value_transform(
-            ecalelf_smearing
-            )
+        mit_smearing = cfg.mit_from_ecalelf_smearing(ecalelf_smearing)
         mit_smearings.append(mit_smearing)
     return ', '.join(mit_smearings)
 
@@ -122,8 +137,50 @@ def parse_scales(cfg):
 
 #_______________________________________________________________________________
 def format_scales(scales_for_run_range, cfg):
-    pass
-    
+    '''
+    Return a list of strings each being a set of argumets of the a scale setter
+    such as the PhotonPairSelector::AddEnCorrPerRun2012HCP(..) given
+    runs->(cat->corr) dictionary of dictionaries scales_for_run_range, which
+    maps a run runge tuple runs = (first, last) to a dictionary, which maps
+    the ECALELF category name (cat) to ECALELF scale corrections.
+    Example of a scales_for_run_range dictionary:
+    {('160431', '165547'): {'EB-absEta_0_1-bad': '0.9961',
+                            'EB-absEta_0_1-gold': '0.9929',
+                            'EB-absEta_1_1.4442-bad': '0.9981',
+                            'EB-absEta_1_1.4442-gold': '0.9883',
+                            'EE-absEta_1.566_2-bad': '0.9961',
+                            'EE-absEta_1.566_2-gold': '0.9944',
+                            'EE-absEta_2_2.5-bad': '0.9987',
+                            'EE-absEta_2_2.5-gold': '0.9941'},
+     ('165548', '167042'): {'EB-absEta_0_1-bad': '0.9971',
+                            'EB-absEta_0_1-gold': '0.9939',
+                            ...}
+     ('167043', '172400'): {'EB-absEta_0_1-bad': '0.9974',
+                            ...},
+     ...}
+     Example of the ouput list:
+     ['160431, 165547, 0.9929, 0.9929, 0.9961, 0.9883, 0.9981, 0.9944, 0.9883, 0.9941, 0.9987',
+      '165548, 167042, 0.9939, 0.9939, 0.9971, 0.9876, 0.9973, 0.9956, 0.9876, 0.9937, 0.9982',
+      '167043, 172400, 0.9942, 0.9942, 0.9974, 0.9883, 0.9981, 0.9956, 0.9883, 0.9948, 0.9993',
+      ...]
+    '''
+    mit_scales = []
+    run_ranges = scales_for_run_range.keys()
+    ## Sort the run ranges in an ascending order.
+    run_ranges.sort(key = lambda x: x[0])
+    ecalelf_from_mit_category = cfg.ecalelf_from_mit_scale_category
+    for run_range in run_ranges:
+        scales_for_category = scales_for_run_range[run_range]
+        mit_scales_per_run_range = []
+        for mit_category in cfg.mit_scale_categories:
+            ecalelf_category = ecalelf_from_mit_category[mit_category]
+            ecalelf_scale = scales_for_category[ecalelf_category]
+            mit_scale = cfg.mit_from_ecalelf_scale(ecalelf_scale)
+            mit_scales_per_run_range.append(mit_scale)
+        args = list(run_range) + mit_scales_per_run_range
+        mit_scales.append(', '.join(args))
+    return mit_scales
+
 #_______________________________________________________________________________
 def read_and_check_corrections(file_name, expected_number_of_lines):
     source_corrections = read_file(file_name).split('\n')
--- Tools/python/update_scales_and_smearings_default_cfg.py
+++ Tools/python/update_scales_and_smearings_default_cfg.py
@@ -2,6 +2,8 @@
 
 import os
 
+#_______________________________________________________________________________
+## INPUT AND OUTPUT FILES
 _base_path = '/home/veverka/cms/cmssw/031/CMSSW_5_3_10_patch1/src/MitHgg/macros'
 
 scales_source_file_name = os.path.join(
@@ -21,21 +23,15 @@ source_macro_file_name = os.path.join(_base_path, 'runHgg2013Final_8TeV_test.C')
 destination_macro_file_name = os.path.join(_base_path,
                                            'runHgg2013Final_8TeV_test_new.C')
 
-## The order, matters here! See PhotonPairSelector::SetMCSmearFactors(...) in
-## UserCode/MitPhysics/Mods/interface/PhotonPairSelector.h
-mit_smearing_categories = '''
-    _EBlowEta_hR9central
-    _EBlowEta_hR9gap
-    _EBlowEta_lR9
-    _EBhighEta_hR9
-    _EBhighEta_lR9
-    _EElowEta_hR9
-    _EElowEta_lR9
-    _EEhighEta_hR9
-    _EEhighEta_lR9
+#_______________________________________________________________________________
+## SCALE CONFIGURATION
+
+## Names of the PhotonPairSelector methods that set the scale corrections.
+scale_setters = '''
+    AddEnCorrPerRun2012HCP
     '''.split()
 
-## The order, matters here! See PhotonPairSelector::SetMCSmearFactors(...) in
+## The order, matters here! See signature of scale_setters in
 ## UserCode/MitPhysics/Mods/interface/PhotonPairSelector.h
 mit_scale_categories = '''
     corr_EBlowEta_hR9central
@@ -49,9 +45,49 @@ mit_scale_categories = '''
     corr_EEhighEta_lR9
     '''.split()
 
+## This is the meat defining the mapping of the scale categories
+## between ECALELF and MIT
+ecalelf_from_mit_scale_category = {
+    'corr_EBlowEta_hR9central' : 'EB-absEta_0_1-gold'     ,
+    'corr_EBlowEta_hR9gap'     : 'EB-absEta_0_1-gold'     ,
+    'corr_EBlowEta_lR9'        : 'EB-absEta_0_1-bad'      ,
+    'corr_EBhighEta_hR9'       : 'EB-absEta_1_1.4442-gold',
+    'corr_EBhighEta_lR9'       : 'EB-absEta_1_1.4442-bad' ,
+    'corr_EElowEta_hR9'        : 'EE-absEta_1.566_2-gold' ,
+    'corr_EElowEta_lR9'        : 'EB-absEta_1_1.4442-gold',
+    'corr_EEhighEta_hR9'       : 'EE-absEta_2_2.5-gold'   ,
+    'corr_EEhighEta_lR9'       : 'EE-absEta_2_2.5-bad'    ,
+    }
+
+## No transform applied.
+mit_from_ecalelf_scale = lambda x: x
+
+#_______________________________________________________________________________
+## SMEARING CONFIGURATION
+
+## Names of the PhotonPairSelector methods that set the smearings.
+smearing_setters = '''
+    SetMCSmearFactors2012HCP
+    SetMCSmearFactors2012HCPMVA
+    '''.split()
+
+## The order, matters here! See the signature of the smearing_setters in
+## UserCode/MitPhysics/Mods/interface/PhotonPairSelector.h
+mit_smearing_categories = '''
+    _EBlowEta_hR9central
+    _EBlowEta_hR9gap
+    _EBlowEta_lR9
+    _EBhighEta_hR9
+    _EBhighEta_lR9
+    _EElowEta_hR9
+    _EElowEta_lR9
+    _EEhighEta_hR9
+    _EEhighEta_lR9
+    '''.split()
+
 ## This is the meat defining the mapping of the smearing
 ## numbers between ECALELF and MIT
-mit_to_ecalelf_smearing_category_map = {
+ecalelf_from_mit_smearing_category = {
     '_EBlowEta_hR9central' : 'EBlowEtaGold' ,
     '_EBlowEta_hR9gap'     : 'EBlowEtaGold' ,
     '_EBlowEta_lR9'        : 'EBlowEtaBad'  ,
@@ -64,15 +100,6 @@ mit_to_ecalelf_smearing_category_map = {
     }
 
 ## ECALELF smearings are in %, MIT expects them in unity.
-ecalelf_to_mit_smearing_value_transform = lambda x: str(0.01 * float(x))
+mit_from_ecalelf_smearing = lambda x: str(0.01 * float(x))
 
-## Names of the PhotonPairSelector methods that set the smearings.
-smearing_setters = '''
-    SetMCSmearFactors2012HCP
-    SetMCSmearFactors2012HCPMVA
-    '''.split()
-
-## Names of the PhotonPairSelector methods that set the scales.
-scale_setters = '''
-    AddEnCorrPerRun2012HCP
-    '''.split()
+  
