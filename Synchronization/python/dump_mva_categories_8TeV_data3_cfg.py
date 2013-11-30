5affd765a28082c10ed7dcaf579915612868fcf2
veverka <>
2013-11-30 11:40:01 +0000

    initial commit

---
+++ Synchronization/python/dump_mva_categories_8TeV_data3_cfg.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+'''
+Configuration for the mva category dump for the 8TeV data trees
+produced by Mingming on 29. November.
+'''
+import glob
+import os
+import sys
+import FWCore.ParameterSet.Config as cms
+from os.path import join
+
+_cmssw_base = os.environ['CMSSW_BASE']
+_input_bases = '''
+    /home/mingyang/cms/hist/hgg-2013Final8TeV/filefi/032/r12a-pho-j22-v1
+    /home/mingyang/cms/hist/hgg-2013Final8TeV/filefi/029/r12b-dph-j22-v1
+    /home/mingyang/cms/hist/hgg-2013Final8TeV/filefi/029/r12c-dph-j22-v1
+    /home/mingyang/cms/hist/hgg-2013Final8TeV/filefi/029/r12d-dph-j22-v1
+    '''.split()
+
+_unmerged_files = []
+for _directory in _input_bases:
+    _files_in_dir = glob.glob(join(_directory, '*.root'))
+    _files_in_dir.sort()
+    _unmerged_files += _files_in_dir
+    _actual = len(_files_in_dir)
+    _expected = int(_files_in_dir[-1].split('_')[-1][:4].lstrip('0')) + 1
+    print >> sys.stderr, '%s: added %3d files' % (_directory, _actual),
+    print >> sys.stderr, 'of %3d expected ...' % _expected
+    
+#_______________________________________________________________________________
+process = cms.Process('DUMP')
+
+#_______________________________________________________________________________
+process.inputs = cms.PSet(
+    fileNames = cms.vstring() + [
+        #join(_input_base, 'hgg-2013Final8TeV_r12%s-j22-v1_noskim.root' % r) 
+        #for r in 'a-pho b-dph c-dph d-dph'.split()
+        ] + _unmerged_files,
+    treeName  = cms.string(
+        '/RunLumiSelectionMod/MCProcessSelectionMod/' +
+        'HLTModP/GoodPVFilterMod/PhotonMvaMod/JetPub/JetCorrectionMod/' +
+        'SeparatePileUpMod/ElectronIDMod/ElectronIDMod/MuonIDMod/MuonIDMod/' +
+        'PhotonPairSelectorPresel/PhotonTreeWriterPresel/' +
+        'hPhotonTree'
+        ),
+    ) ## process.inputs
+
+#_______________________________________________________________________________
+process.dump = cms.PSet(
+    diphotonMvaWeights = cms.string(
+        join(_cmssw_base, 'src/MitPhysics/data/',
+             'HggBambu_SMDipho_Oct01_redqcdweightallsigevenbkg_BDTG.' +
+                 'weights.xml')
+        ),
+    dijetMvaWeights    = cms.string(
+        join(_cmssw_base, 'src/MitHgg/PhotonTree/data',
+             'TMVA_dijet_sherpa_scalewt50_2evenb_powheg200_maxdPhi_oct9_' +
+                 'Gradient.weights.xml')
+        ),
+    combinedMvaWeights = cms.string(
+        join(_cmssw_base, 'src/MitHgg/PhotonTree/data',
+             'TMVA_vbf_dijet_dipho_evenbkg_scaledwt50_maxdPhi_Gradient.' +
+                 'weights.xml')
+        ),
+    diphotonMvaUseSmearedMassError = cms.bool(True),
+    dijetMvaMaxDPhi = cms.double(2.916),
+    ## 8TeV categories by Josh from 21 November 2013
+    diphotonBdtBoundaries = cms.vdouble(0.76, 0.36, 0.00, -0.42, -0.78),
+    dijetBdtBoundaries    = cms.vdouble(),
+    combinedBdtBoundaries = cms.vdouble(0.94, 0.82, 0.14),
+    ) ## process.dump
+
+
+#_______________________________________________________________________________
+if __name__ == '__main__':
+    import user
