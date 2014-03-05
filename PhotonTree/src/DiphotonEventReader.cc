3987e821355b47414fee53496a11583f4e0118bd
Jan Veverka <jan.veverka@gmail.com>
2014-03-05 11:46:10 -0500

    Removed unneeded dependancies

--- PhotonTree/src/DiphotonEventReader.cc
+++
@@ -1,2 +0,0 @@
-#include "MitHgg/PhotonTree/interface/DiphotonEventReader.h"
-ClassImp(::mithep::hgg::DiphotonEventReader)
--- macros/sorttxt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat $1 | cut -f 1,2 -d "*" --complement | sort > $2
