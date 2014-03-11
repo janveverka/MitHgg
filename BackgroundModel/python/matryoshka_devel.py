ba62d7c75944490a149d597600a1fc772cfd35a6
Jan Veverka <jan.veverka@gmail.com>
2014-03-11 18:41:11 +0100

    First crack at OOP

--- BackgroundModel/python/matryoshka_devel.py
+++ BackgroundModel/python/matryoshka_devel.py
@@ -14,52 +14,99 @@ the matryoshka name.
 
 Plan: use pow for f and chebyshev for g.
 '''
+import array
 import ROOT
 import FWLite.Tools.roofit as roo
 import FWLite.Tools.canvases as canvases
 
 plots = []
 
-# Let's first try the probabilty transform for a binned dataset.
-w = ROOT.RooWorkspace('w')
+## Let's first try the probabilty transform for a binned dataset.
+## The original variable is xvar, the transformed une is uvar (for uniform).
+
+#===============================================================================
+class MatroyshkaTest:
+    #___________________________________________________________________________
+    def __init__(self, nevents=1000):
+        self.plots = []
+        self.w = w = ROOT.RooWorkspace('w')
+        self.gauss = w.factory('''Gaussian::gauss(xvar[-5,5],mean[0,-5,5],
+                                                  sigma[1,0.1,10])''')
+        self.xvar = w.var('xvar')
+        self.xset = ROOT.RooArgSet(self.xvar)
+        self.data = self.gauss.generateBinned(self.xset, nevents)
+        self._init_cdf()
+        self._init_inverse_cdf()
+        self._init_uvar()
+        self._init_titles()
+
+    #___________________________________________________________________________
+    def _init_cdf(self, scan_bins = 1000, scan_order = 2):
+        scan_pars = roo.ScanParameters(scan_bins, scan_order)
+        self.gauss_cdf = self.gauss.createCdf(self.xset, scan_pars)
+
+    #___________________________________________________________________________
+    def _init_inverse_cdf(self, inverse_precision = 1e-4):
+        self.gauss_cdf_inverse = ROOT.RooNumInverse(
+            'gauss_cdf_inverse',
+            'Inverse of ' + self.gauss_cdf.GetTitle(),
+            self.xvar, self.gauss_cdf, inverse_precision
+            )
+
+    #___________________________________________________________________________
+    def _init_uvar(self):
+        self.uvar = self.w.factory('uvar[0.5, 0, 1]')
+        xbinning = self.xvar.getBinning()
+        nbins = xbinning.numBins()
+        uedges = []
+        for iedge in range(nbins + 1):
+            self.xvar.setVal(xbinning.array()[iedge])
+            uedges.append(self.gauss_cdf.getVal())
+        uboundaries = array.array('d', uedges)
+        ubinning = ROOT.RooBinning(nbins, uboundaries)
+        self.uvar.setBinning(ubinning)
+
+    #___________________________________________________________________________
+    def _init_titles(self):
+        self.xvar.SetTitle('x')
+        self.uvar.SetTitle('u')
+    #___________________________________________________________________________
+    def run(self):
+        self.fit_toy()
+
+    #___________________________________________________________________________
+    def fit_toy(self):
+        plot = self.xvar.frame(roo.Title('Gaussian Toy'))
+        self.plots.append(plot)
+        self.data.plotOn(plot)
+        self.gauss.plotOn(plot)
+        self.gauss.fitTo(self.data)
+        self.gauss.plotOn(plot, roo.LineColor(ROOT.kRed))
+        canvases.next('GaussianToy')
+        plot.Draw()
+        ## FIXME: Add legend a la http://root.cern.ch/phpBB3/viewtopic.php?p=31694
+# End of MatroyshkaTest
+
+#===============================================================================
+def main():
+    global mtest
+    mtest = MatroyshkaTest()
+    mtest.run()
+    canvases.update()
+## End of main
 
 
-gauss = w.factory('Gaussian::gauss(xvar[-5,5],mean[0,-5,5],sigma[1,0.1,10])')
-xvar = w.var('xvar')
-observables = ROOT.RooArgSet(xvar)
-data = gauss.generateBinned(observables, 1000)
+#plot = xvar.frame()
+#plots.append(plot)
+#gauss.plotOn(plot)
+#gauss_cdf.plotOn(plot, roo.LineColor(ROOT.kRed))
+##gauss_cdf_inverse.plotOn(plot, roo.Range(0,1), roo.LineStyle(ROOT.kDashed))
+#canvases.next('cdf')
+#plot.Draw()
 
-scan_bins = 1000
-scan_order = 2
-inverse_precision = 1e-4
-scan_pars = roo.ScanParameters(scan_bins, scan_order)
-gauss_cdf = gauss.createCdf(observables, scan_pars)
-gauss_cdf_inverse = ROOT.RooNumInverse(
-	'gauss_cdf_inverse',
-	'Inverse of ' + gauss_cdf.GetTitle(),
-	xvar, gauss_cdf, inverse_precision
-	)
-
-plot = xvar.frame()
-plots.append(plot)
-data.plotOn(plot)
-gauss.plotOn(plot)
-gauss.fitTo(data)
-gauss.plotOn(plot, roo.LineColor(ROOT.kRed))
-
-canvases.next('fit')
-plot.Draw()
-
-plot = xvar.frame()
-plots.append(plot)
-gauss.plotOn(plot)
-gauss_cdf.plotOn(plot, roo.LineColor(ROOT.kRed))
-#gauss_cdf_inverse.plotOn(plot, roo.Range(0,1), roo.LineStyle(ROOT.kDashed))
-canvases.next('cdf')
-plot.Draw()
-
-yvar = w.factory('yvar[0.5, 0, 1]')
-
-canvases.update()
+#canvases.update()
 
 
+#===============================================================================
+if __name__ == '__main__':
+    main()
